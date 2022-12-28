0e6a0f6b28c20d69b594ade7d6c6e5f93d37309f
aniket-mantra <aniket.s@mantralabsglobal.com>
2022-12-28 18:00:26 +0530

    new changes for VideoCat:

--- src/components/VideoCategoryHome.js
+++ src/components/VideoCategoryHome.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "../css/style.css";
-import Table from "react-bootstrap/Table";
 import Cookies from "js-cookie";
 import axios from "axios";
 import { ENDPOINTS } from "../helper/endpoints";
-import DataTable from "react-data-table-component";
 import { Container, Card, Col, Button } from "react-bootstrap";
 import Swal from "sweetalert2";
+import backImg from "../asserts/back-button.png";
+
 function VideoCategoryHome() {
   const userAuthToken = Cookies.get("authToken");
   const [videoCategory, setVideoCategory] = useState([]);
-  const [videoCatId, setVideoCatId] = useState("")
-  const [categoryName, setCategoryName] = useState("")
-  const [categoryDesc, setCategoryDesc] = useState("")
-  const [isCategoryActive, setCategoryActive] = useState(true)
-  const [openUpdateCat, setOpenUpdateCat] = useState(false)
+  const [videoCatId, setVideoCatId] = useState("");
+  const [categoryName, setCategoryName] = useState("");
+  const [categoryDesc, setCategoryDesc] = useState("");
+  const [isCategoryActive, setCategoryActive] = useState(true);
+  const [openUpdateCat, setOpenUpdateCat] = useState(false);
 
   const headers = {
     "Content-Type": "application/json",
@@ -23,6 +23,7 @@ function VideoCategoryHome() {
 
   useEffect(() => {
     fetchAllVideoCat();
+    console.log(isCategoryActive);
   }, []);
 
   const fetchAllVideoCat = () => {
@@ -38,75 +39,165 @@ function VideoCategoryHome() {
   };
 
   const deleteVideoCat = (cat_id) => {
-    
-    axios.delete(ENDPOINTS.DELETE_VIDEO_CATEGORY+"?"+"category_id="+cat_id, {
-      headers: {
-        Authorization: userAuthToken
-      }
-    })
-    .then((response) => {
-      Swal.fire({
-        title: "Success",
-        text: response.data.message,
-        icon: "success",
-        confirmButtonText: "OK",
+    axios
+      .delete(ENDPOINTS.DELETE_VIDEO_CATEGORY + "?" + "category_id=" + cat_id, {
+        headers: {
+          Authorization: userAuthToken,
+        },
+      })
+      .then((response) => {
+        Swal.fire({
+          title: "Success",
+          text: response.data.message,
+          icon: "success",
+          confirmButtonText: "OK",
+        });
+        setVideoCategory((current) =>
+          current.filter((videoCategory) => {
+            return videoCategory.id !== cat_id;
+          })
+        );
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops..." + error.response.data.error,
+          text: "Something went wrong!",
+        });
       });
-      setVideoCategory(current => current.filter(videoCategory => {
-        return videoCategory.id != cat_id;
-      }))
-    })
-    .catch((error) => {
-      Swal.fire({
-        icon: "error",
-        title: "Oops..." + error.response.data.error,
-        text: "Something went wrong!",
-      });
-    })
   };
 
-  const openForUpdate = (obj) => {
-    setVideoCatId(obj.id)
-    setCategoryName(obj.category_name)
-    setCategoryDesc(obj.category_desc)
-    setCategoryActive(obj.is_category_active)
-    setOpenUpdateCat(true)
+  const updateVideoCat = () => {
+    console.log("radio button", isCategoryActive);
   }
+
+  const openForUpdate = (obj) => {
+    setVideoCatId(obj.id);
+    setCategoryName(obj.category_name);
+    setCategoryDesc(obj.category_desc);
+    setCategoryActive(obj.is_category_active);
+    setOpenUpdateCat(true);
+  };
+
+  const _handleInputChange = (e) =>{
+    var isChecked = e.target.value ==="1" ? this.state.radioButton1 : this.state.radioButton2;
+    this.props.handleChange(e);
+}
+
+
+  const openForAllData = () => {
+    setOpenUpdateCat(false);
+  };
 
   return (
     <>
       <div className="App">
-        <Container className="p-4">
-          {videoCategory.map((item) => (
-            <Col className="card-videocat">
-              <Card className="card-videocat">
-                <Card.Img variant="top" />
-                <Card.Body>
-                  <Card.Title>{item.category_name}</Card.Title>
-                  <Card.Text>{item.category_desc}</Card.Text>
-                  {item.is_category_active ? (
-                    <Card.Subtitle style={{ color: "green" }}>
+        {openUpdateCat ? (
+          <>
+            <img
+            alt="#"
+              className="back-img-video-cat"
+              src={backImg}
+              onClick={() => openForAllData()}
+            />
+            <Container className="p-4">
+              <Col className="card-videocat">
+                <Card className="card-videocat">
+                  <Card.Img variant="top" />
+                  <Card.Body>
+                    <Card.Title>
+                      <div>
+                        <label>Category Name</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Enter video category name"
+                          value={categoryName}
+                          onChange={(e) => setCategoryName(e.target.value)}
+                        />
+                      </div>
+                    </Card.Title>
+                    <Card.Text>
+                      <div>
+                        <label>Category Description</label>
+                        <textarea
+                          type="textarea"
+                          className="form-control"
+                          placeholder="Enter video category description"
+                          value={categoryDesc}
+                          cols={40} 
+                          rows={10}
+                          onChange={(e) => setCategoryName(e.target.value)}
+                        />
+                      </div>
+                    </Card.Text>
+                    <div onChange={(e) => setCategoryActive(e.target.value)}>
+                      <input
+                        className="radio"
+                        type="radio"
+                        checked={isCategoryActive === true}
+                        value={true}
+                        onChange={(e) => setCategoryActive(e.target.value)}
+                        name="gender"
+                      />{" "}
                       Active
-                    </Card.Subtitle>
-                  ) : (
-                    <Card.Subtitle style={{ color: "red" }}>
-                      Not Active
-                    </Card.Subtitle>
-                  )}
+                      <input
+                        className="radio"
+                        type="radio"
+                        checked={isCategoryActive === false}
+                        value={false}
+                        onClick={(e) => setCategoryActive(e.target.value)}
+                        name="gender"
+                      />{" "}
+                      DeActive
+                    </div>
 
-                  <Button variant="primary" style={{ margin: "10px" }} onClick={() => openForUpdate(item)}>
-                    Update{" "}
-                  </Button>
-                  <Button
-                    variant="primary"
-                    onClick={() => deleteVideoCat(item.id)}
-                  >
-                    Delete{" "}
-                  </Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Container>
+                    <Button variant="primary" style={{ margin: "10px" }} onClick={() => updateVideoCat()}>
+                      Update{" "}
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Container>
+          </>
+        ) : (
+          <Container className="p-4">
+            {videoCategory.map((item) => (
+              <Col className="card-videocat">
+                <Card className="card-videocat">
+                  <Card.Img variant="top" />
+                  <Card.Body>
+                    <Card.Title>{item.category_name}</Card.Title>
+                    <Card.Text>{item.category_desc}</Card.Text>
+                    {item.is_category_active ? (
+                      <Card.Subtitle style={{ color: "green" }}>
+                        Active
+                      </Card.Subtitle>
+                    ) : (
+                      <Card.Subtitle style={{ color: "red" }}>
+                        Not Active
+                      </Card.Subtitle>
+                    )}
+
+                    <Button
+                      variant="primary"
+                      style={{ margin: "10px" }}
+                      onClick={() => openForUpdate(item)}
+                    >
+                      Update{" "}
+                    </Button>
+                    <Button
+                      variant="primary"
+                      onClick={() => deleteVideoCat(item.id)}
+                    >
+                      Delete{" "}
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Container>
+        )}
       </div>
     </>
   );
